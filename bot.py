37ccaad245aeb5ba0c513b7a4451e89d8944df5a
qiwihui <qwh005007@gmail.com>
2022-04-29 15:47:19 +0800

    feat: add ceate-channel

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import os
 import random
-
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -25,6 +25,7 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+
 @bot.command(name='roll_dice', help='Simulates rolling dice')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
@@ -32,5 +33,31 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
+
+@bot.command(name='ceate-channel', help='Creates a new text channel')
+async def create_channel(ctx, channel_name: str):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+    else:
+        print(f'Channel {channel_name} already exists')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await ctx.send('Command not found')
+
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have permission to use this command')
+
 
 bot.run(TOKEN)
