8f038a8b157d3ebb412732957733849b2898a1d1
qiwihui <qwh005007@gmail.com>
2022-04-29 15:35:41 +0800

    feat: add roll dice

--- bot.py
+++ bot.py
@@ -25,4 +25,12 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+@bot.command(name='roll_dice', help='Simulates rolling dice')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
 bot.run(TOKEN)
